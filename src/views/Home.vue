870dc483a71d54bf5460bce82569ac140b945040
DariusMargarit <margaritdariu89@yahoo.com>
2021-04-08 20:06:56 +0300

    homepage updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <v-main class="main">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
         <Navbar />
 
         <div class="first_content">
@@ -25,47 +25,60 @@
             </div>
         </div>
 
-        <br />
         <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>
         <br />
-        <div style="align-items: center; justify-items: center; align-content: center; justify-content: center; display:flex"><h1 style="margin-left: 40px">Ce este HonestReviews?</h1></div>
-        <br />
-        <div style="align-items: center; justify-items: center; align-content: center; justify-content: center; display:flex">
-            <br />
-            <div class="columntext">
-                <h3 class="paragraph">
-                    HonestReviews este un website prin intermediul caruia utilizatorii pot lasa review-uri, adica pareri, oricarui produs pe care l-au achizitionat.
-                    Un asa-zis "review" consta in exprimarea propriei opinii in legatura cu produsul ales.
-                    De asemenea, sunt mentionate sursa de unde a fost achizitionat produsul, numele intreg al acestuia, pretul si alte informatii utile.
-                </h3>
+
+
+
+        <div class="content-part">
+            <div class="content-box">
+                <div class="content-title">
+                    <h1>Ce este HonestReviews?</h1>
+                </div>
+
+                <div class="content-parag">
+                    <p>
+                        HonestReviews este un website prin intermediul caruia utilizatorii pot lasa review-uri, adica pareri, oricarui produs pe care l-au achizitionat.
+                        Un asa-zis "review" consta in exprimarea propriei opinii in legatura cu produsul ales.
+                        De asemenea, sunt mentionate sursa de unde a fost achizitionat produsul, numele intreg al acestuia, pretul si alte informatii utile.
+                    </p>
+                </div>
             </div>
-            <div class="columnimg">
-                <!--          <img src="../assets/desk.png" class="image" />-->
+
+            <div class="content-image">
+                <img src="../assets/social-media-3846597_640.png" />
             </div>
+
         </div>
+
+
         <br />
         <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-right</v-icon></div>
         <br />
 
-        <div class="poly1">
+        <div class="content-part">
 
-        </div>
+            <div class="content-image mr-12">
+                <img style="background: white" src="../assets/social-media-5187243_1280.png"  />
+            </div>
 
-        <div style="align-items: center; justify-items: center; align-content: center; justify-content: center; display:flex">
-            <h1 style="margin-left: 40px">De ce HonestReviews?</h1></div>
-        <br />
-        <div style="align-items: center; justify-items: center; align-content: center; justify-content: center; display:flex">
-            <br />
-            <div class="columntext">
-                <h3 class="paragraph">
-                    Site-ul nostru va ofera o modalitate usoara de a scrie sau de a cauta impresii ale altor utilizatori despre orice fel ce produs.
-                    De la frigidere la computere, de la masini la biciclete, de la resorturi turistice la restaurante, aici puteti gasi un review pentru orice categorie imaginabila.
-                    Datorita interfatei construita sa fie usor de folosit, oricine poate beneficia de acest site.
-                    In plus, autentificarea se poate realiza prin intermediul E-Mailului, al contului de Google sau al contului de Facebook, asigurand, astfel, un proces rapid de logare.
-                </h3>
+            <div class="content-box mr-15">
+                <div class="content-title">
+                    <h1>De ce HonestReviews?</h1>
+                </div>
+
+                <div class="content-parag">
+                    <p>
+                        Site-ul nostru va ofera o modalitate usoara de a scrie sau de a cauta impresii ale altor utilizatori despre orice fel ce produs.
+                        De la frigidere la computere, de la masini la biciclete, de la resorturi turistice la restaurante, aici puteti gasi un review pentru orice categorie imaginabila.
+                        Datorita interfatei construita sa fie usor de folosit, oricine poate beneficia de acest site.
+                        In plus, autentificarea se poate realiza prin intermediul E-Mailului, al contului de Google sau al contului de Facebook, asigurand, astfel, un proces rapid de logare.
+                    </p>
+                </div>
             </div>
 
         </div>
+
     </v-main>
 </template>
 
@@ -101,12 +114,14 @@
         font-size: 34px;
         font-weight: 800;
         padding: 25px 15px;
+        font-family: 'Archivo Black', sans-serif;
     }
 
     .subcontent-text {
         font-size: 34px;
         font-weight: 800;
         margin-bottom: 50px;
+        font-family: 'Archivo Black', sans-serif;
     }
 
     .content {
@@ -129,43 +144,6 @@
         flex-basis: 31%;
     }
 
-
-
-    .columntext {
-        align-items: center;
-        justify-items: center;
-        align-content: center;
-        justify-content: center;
-        display: flex;
-        float: left;
-        width: 40%;
-        column-gap: 50px;
-        padding-left: 40px;
-    }
-
-    .columnimg {
-        align-items: center;
-        justify-items: center;
-        align-content: center;
-        justify-content: center;
-        display: flex;
-        float: left;
-        width: 40%;
-        column-gap: 50px;
-        padding-left: 70px;
-    }
-
-    .img {
-        width: 100%;
-        height: 100%;
-    }
-
-    .buttons {
-        font-family: 'Lato', sans-serif;
-        font-weight: bold;
-
-    }
-
     .separator {
         display: flex;
         align-items: center;
@@ -185,13 +163,43 @@
         margin-left: .25em;
     }
 
-    .poly1 {
-        clip-path: polygon(35% 68%, 100% 68%, 75% 100%, 0% 100%);
-        background-color: #ee82ee;
-        position: absolute;
-        width:100%;
+    .content-part{
+        flex-basis: 31%;
+        justify-content: space-between;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        padding: 40px;
+        margin: 30px;
+    }
+
+    .img {
+        width: 100%;
         height: 100%;
-        z-index: -1;
+    }
+
+    .content-box{
+        width: 40%;
+        padding: 20px;
+        margin: 5px 0px 5px 15px;
+        text-align: center;
+
+    }
+
+    .content-title{
+        margin-bottom: 15px;
+        font-weight: 900;
+        font-family: 'Archivo Black', sans-serif;
+    }
+
+    .content-parag{
+        font-size: 20px;
+        font-weight: 200;
+    }
+
+    .content-image{
+        margin-right: 30px;
+        padding-right: 20px;
     }
 
 
