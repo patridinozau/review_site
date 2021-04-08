ea5081970146ebe32661e7550420991bf9fa17a2
Robert <gaspar.robert16@gmail.com>
2021-04-08 22:07:28 +0300

    Img profil si aliniere

---
+++ src/views/User.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-main class="main">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <br><br>
+    <div class="profileCard" >
+      <table>
+        <tr>
+          <td>
+            <img src="https://i0.wp.com/365webresources.com/wp-content/uploads/2016/09/FREE-PROFILE-AVATARS.png?resize=502%2C494&ssl=1" width="300" height="300" class="profilePic">
+            <p>lalalalal</p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <p>lalalalal</p>
+            <br>
+            <br>
+          </td>
+          <td>
+            <
+          </td>
+        </tr>
+        <tr>
+          <H4>Descriere pt persoana</H4>
+        </tr>
+      </table>
+    </div>
+    <br><br>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "User"
+}
+</script>
+
+<style scoped>
+.main {
+  background-color: #f2ffff;
+  font-family: 'Lato', sans-serif;
+}
+.profileCard {
+  position: center;
+  margin: auto;
+  /*display:flex;
+  justify-content: space-between;*/
+  top: 5px;
+  left: 0;
+  width: 85%;
+  padding: 20px;
+  background-color: #ffffff;
+  border-radius: 10px;
+}
+.profilePic{
+  border-radius: 10px;
+}
+</style>
