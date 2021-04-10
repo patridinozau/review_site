62d967c9a5a86f98cac81862421849b74c43bf0d
3 <77794481+smallpixel1105@users.noreply.github.com>
2021-04-10 23:56:53 +0300

    cards still need pics + new bg

--- src/views/despreNoi.vue
+++ src/views/despreNoi.vue
@@ -15,48 +15,110 @@
       <div class="grad container">
         <br />
         <v-card class="card">
-          <h3>Darius</h3>
+          <v-card-text class="card_text">
+              <div align="center" style="font-weight:800; font-size: 34px;" class="font">
+                  <span class="font" style="font-weight:800; font-size: 34px;">- Darius -</span>
+              </div>
+            <br />
+              <div>
+                  <span class="font" style="font-weight: 200; font-size: 17px; align-content:center; display: flex;">Calitati: </span>
+                  <ul style="list-style-position: inside; list-style-type: square">
+                    <li>ambitios</li>
+                    <li>bun lider</li>
+                    <li>foarte muncitor</li>
+                  </ul>
+              </div>
+              <div>
+                <h3>Fun fact:</h3>
+              </div>
+          </v-card-text>
         </v-card>
         <v-card class="card">
-          <h3>Robert</h3>
+          <v-card-text class="card_text">
+            <div align="center" style="font-weight:800; font-size: 34px;" class="font">
+              <span class="font" style="font-weight:800; font-size: 34px;">- Iulian -</span>
+            </div>
+            <br />
+            <div>
+              <span class="font" style="font-weight: 200; font-size: 17px; align-content:center; display: flex;">Calitati: </span>
+              <ul style="list-style-position: inside; list-style-type: square">
+                <li>harnic</li>
+                <li>respectuos</li>
+                <li>inteligent</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Fun fact:</h3>
+            </div>
+          </v-card-text>
         </v-card>
         <v-card class="card">
-          <h3>Iulian</h3>
+          <v-card-text class="card_text">
+            <div align="center" style="font-weight:800; font-size: 34px;" class="font">
+              <span class="font" style="font-weight:800; font-size: 34px;">- Robert -</span>
+            </div>
+            <br />
+            <div>
+              <span class="font" style="font-weight: 200; font-size: 17px; align-content:center; display: flex;">Calitati: </span>
+              <ul style="list-style-position: inside; list-style-type: square">
+                <li>destept</li>
+                <li>atent</li>
+                <li>meticulos</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Fun fact:</h3>
+            </div>
+          </v-card-text>
         </v-card>
         <v-card class="card">
-          <h3>Patri</h3>
+          <v-card-text class="card_text">
+            <div align="center" style="font-weight:800; font-size: 34px;" class="font">
+              <span class="font" style="font-weight:800; font-size: 34px;">- Patri -</span>
+            </div>
+            <br />
+            <div>
+              <span class="font" style="font-weight: 200; font-size: 17px; align-content:center; display: flex;">Calitati: </span>
+              <ul style="list-style-position: inside; list-style-type: square">
+                <li>ingenioasa</li>
+                <li>silitoare</li>
+                <li>creativa</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Fun fact: stie sa cante la ukulele</h3>
+            </div>
+          </v-card-text>
         </v-card>
+      </div>
+      <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>
+      <div>
+
       </div>
     </div>
   </v-main>
 </template>
 
 <script>
-
-
 export default {
-
 }
-
 </script>
 
 <style>
-
 .font {
-  font-family: 'Lato', sans-serif;
+  font-family: 'Archivo Black', sans-serif;
 }
-
 .grad {
   background: rgb(255,255,255);
-  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(187,196,189,1) 100%);
+  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,231,1) 23%, rgba(195,205,197,1) 100%);
+  /*background: rgb(255,255,255);*/
+  /*background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,231,1) 25%, rgba(195,205,197,1) 50%, rgba(255,255,231,1) 75%, rgba(255,255,255,1) 100%);*/
 }
-
 .container {
   display:flex;
   align-content: center;
-  jutify-content: center;
+  justify-content: center;
 }
-
 .card {
   width: 40%;
   height: 40%;
@@ -67,6 +129,12 @@ export default {
   display: flex;
 }
 
+/*.card_text {*/
+/*  align-content: center;*/
+/*  justify-content: center;*/
+/*  display: flex;*/
+/*}*/
+
 .title {
   margin-bottom: 15px;
   font-size: 200px;
@@ -74,6 +142,25 @@ export default {
   padding: 25px 15px;
   font-family: 'Archivo Black', sans-serif;
   position: center;
+}
+
+.separator {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-family: 'Arvo', serif;
+}
+.separator::before,
+.separator::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid #e0eaf5;
+}
+.separator:not(:empty)::before {
+  margin-right: .25em;
+}
+.separator:not(:empty)::after {
+  margin-left: .25em;
 }
 
 </style>
