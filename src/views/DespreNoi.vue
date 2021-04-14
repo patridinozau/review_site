1ac26becf497d3cd961452a5fc5858caa61cd2b0
DariusMargarit <margaritdariu89@yahoo.com>
2021-04-14 17:08:06 +0300

    big merge cu toate branch-urile manual, ca nu se putea pe github

--- src/views/DespreNoi.vue
+++ src/views/DespreNoi.vue
@@ -3,11 +3,36 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
-    <nav-bar />
-    <div>
+    <navbar />
+    <br />
+    <br />
+    <div class="font">
       <br />
-      <br />
-      <br />
+      <br /> <br /> <br />
+      <div>
+        <div class="content-part">
+          <div class="content-box">
+            <div class="content-title">
+              <h1 class="font">Care este scopul site-ului nostru?</h1>
+            </div>
+
+            <div class="content-parag">
+              <p>
+                De la inceputul pandemiei de covid-19, viata fiecaruia dintre noi s-a schimbat radical.
+                Odata cu implementarea masurilor de siguranta, cu totii am inceput sa facem cumparaturi online mai des.
+                Astfel, consideram ca este benefica existenta unui spatiu virtual unde oricine poate lasa sau gasi o recenzie.
+              </p>
+            </div>
+          </div>
+
+          <div class="content-image">
+            <img src="../assets/team-meeting-illustration.png" />
+          </div>
+
+        </div>
+      </div>
+
+      <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-right</v-icon></div>
       <br />
       <div class="title" align="center">
         <h1 >Cine sunt membrii echipei?</h1>
@@ -16,27 +41,29 @@
         <br />
         <v-card class="card">
           <v-card-text class="card_text">
-              <div align="center" style="padding:15px;font-weight:800; font-size: 34px;" class="font">
-                  <span class="font" style="padding:15px; font-weight:800; font-size: 34px;">- Darius -</span>
-              </div>
+            <div align="center" style="padding:15px;font-weight:800; font-size: 34px;" class="font">
+              <span class="font" style="padding:15px; font-weight:800; font-size: 34px;">- Darius -</span>
+            </div>
             <br />
-              <div align="center">
-                <v-avatar size="130" style="">
-                  <img src="../assets/darius.jpg" />
-                </v-avatar>
-              </div>
+            <div align="center">
+              <v-avatar size="130" style="">
+                <img src="../assets/darius.jpg" />
+              </v-avatar>
+            </div>
             <br />
-              <div>
-                  <span class="font" style="padding:15px;font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă l-au descris: </span>
-                  <ul style="list-style-position: inside; list-style-type: square">
-                    <li>ambițios</li>
-                    <li>bun lider</li>
-                    <li>foarte muncitor</li>
-                  </ul>
-              </div>
-              <div>
-                <h3>Fun fact:</h3>
-              </div>
+            <div>
+              <span class="font" style="padding-bottom:15px;font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă l-au descris: </span>
+              <ul style="list-style-position: inside; list-style-type: square">
+                <li>ambițios</li>
+                <li>bun lider</li>
+                <li>foarte muncitor</li>
+              </ul>
+            </div>
+            <br />
+            <div>
+              <h3>Fun fact: știe să se dea bine cu patinele</h3>
+            </div>
+            <br />
           </v-card-text>
         </v-card>
         <v-card class="card">
@@ -47,20 +74,21 @@
             <br />
             <div align="center">
               <v-avatar size="130" style="">
-                <img src="../assets/darius.jpg" class />
+                <img src="../assets/iulian.jpeg" class />
               </v-avatar>
             </div>
             <br />
             <div>
-              <span class="font" style="padding:15px;font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă l-au descris: </span>
+              <span class="font" style="padding-bottom:15px;font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă l-au descris: </span>
               <ul style="list-style-position: inside; list-style-type: square">
                 <li>harnic</li>
                 <li>respectuos</li>
                 <li>inteligent</li>
               </ul>
             </div>
+            <br />
             <div>
-              <h3>Fun fact:</h3>
+              <h3 >Fun fact: știe să facă scheme cu bicicleta</h3>
             </div>
           </v-card-text>
         </v-card>
@@ -77,16 +105,18 @@
             </div>
             <br />
             <div>
-              <span class="font" style="padding:15px;font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă l-au descris: </span>
+              <span class="font" style="padding-bottom:15px;font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă l-au descris: </span>
               <ul style="list-style-position: inside; list-style-type: square">
                 <li>deștept</li>
                 <li>atent</li>
                 <li>meticulos</li>
               </ul>
             </div>
+            <br />
             <div>
-              <h3>Fun fact:</h3>
+              <h3>Fun fact: îi place să schieze</h3>
             </div>
+            <br />
           </v-card-text>
         </v-card>
         <v-card class="card">
@@ -102,134 +132,107 @@
             </div>
             <br />
             <div>
-              <span class="font" style="padding:15px; font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă au descris-o: </span>
+              <span class="font" style="padding-bottom:15px; font-weight: 200; font-size: 17px; align-content:center; display: flex;">Trei cuvinte prin care colegii de echipă au descris-o: </span>
               <ul style="list-style-position: inside; list-style-type: square">
                 <li>perfecționistă</li>
                 <li>motivată</li>
                 <li>creativă</li>
               </ul>
             </div>
+            <br />
             <div>
               <h3>Fun fact: știe să cânte la ukulele</h3>
             </div>
+            <br />
           </v-card-text>
         </v-card>
-      </div>
-      <div style="padding:20px" class="separator"><v-icon>mdi-chevron-triple-left</v-icon></div>
-      <div>
-        <div class="content-part">
-          <div class="content-box">
-            <div class="content-title">
-              <h1>Care este scopul site-ului nostru?</h1>
-            </div>
-
-            <div class="content-parag">
-              <p>
-                  De la inceputul pandemiei de covid-19, viata fiecaruia dintre noi s-a schimbat radical.
-
-              </p>
-            </div>
-          </div>
-
-          <div class="content-image">
-            <img src="../assets/team-meeting-illustration.png" />
-          </div>
-
-        </div>
       </div>
     </div>
   </v-main>
 </template>
 
 <script>
-
   import Navbar from "../components/Navbar";
 
   export default {
-    name: 'DespreNoi',
     components: {
-      'nav-bar': Navbar
+      'navbar': Navbar
     }
-}
+  }
 </script>
 
 <style>
-.font {
-  font-family: 'Archivo Black', sans-serif;
-}
-.grad {
-  background: rgb(255,255,255);
-  background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,231,1) 23%, rgba(195,205,197,1) 100%);
-  /*background: rgb(255,255,255);*/
-  /*background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,231,1) 25%, rgba(195,205,197,1) 50%, rgba(255,255,231,1) 75%, rgba(255,255,255,1) 100%);*/
-}
-.container {
-  display:flex;
-  align-content: center;
-  justify-content: center;
-}
-.card {
-  width: 40%;
-  height: 40%;
-  padding: 20px;
-  margin: 20px;
-  align-content: center;
-  justify-content: center;
-  display: flex;
-}
-
-/*.card_text {*/
-/*  align-content: center;*/
-/*  justify-content: center;*/
-/*  display: flex;*/
-/*}*/
-
-.title {
-  margin-bottom: 15px;
-  font-size: 200px;
-  font-weight: 800;
-  padding: 25px 15px;
-  font-family: 'Archivo Black', sans-serif;
-  position: center;
-}
-
-.separator {
-  display: flex;
-  align-items: center;
-  text-align: center;
-  font-family: 'Arvo', serif;
-}
-.separator::before,
-.separator::after {
-  content: '';
-  flex: 1;
-  border-bottom: 1px solid #e0eaf5;
-}
-.separator:not(:empty)::before {
-  margin-right: .25em;
-}
-.separator:not(:empty)::after {
-  margin-left: .25em;
-}
-
-.content-box{
-  width: 40%;
-  padding: 20px;
-  margin: 5px 0px 5px 15px;
-  text-align: center;
-}
-.content-title{
-  margin-bottom: 15px;
-  font-weight: 900;
-  font-family: 'Archivo Black', sans-serif;
-}
-.content-parag{
-  font-size: 20px;
-  font-weight: 100;
-}
-.content-image{
-  margin-right: 30px;
-  padding-right: 20px;
-}
-
+  .font {
+    font-family: 'Archivo Black', sans-serif;
+  }
+  .grad {
+    background: rgb(255,255,255);
+    background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,231,1) 23%, rgba(195,205,197,1) 100%);
+    /*background: rgb(255,255,255);*/
+    /*background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,231,1) 25%, rgba(195,205,197,1) 50%, rgba(255,255,231,1) 75%, rgba(255,255,255,1) 100%);*/
+  }
+  .container {
+    display:flex;
+    align-content: center;
+    justify-content: center;
+  }
+  .card {
+    width: 40%;
+    height: 40%;
+    padding: 20px;
+    margin: 20px;
+    align-content: center;
+    justify-content: center;
+    display: flex;
+  }
+  /*.card_text {*/
+  /*  align-content: center;*/
+  /*  justify-content: center;*/
+  /*  display: flex;*/
+  /*}*/
+  .title {
+    margin-bottom: 15px;
+    font-size: 200px;
+    font-weight: 800;
+    padding: 25px 15px;
+    font-family: 'Archivo Black', sans-serif;
+    position: center;
+  }
+  .separator {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    font-family: 'Arvo', serif;
+  }
+  .separator::before,
+  .separator::after {
+    content: '';
+    flex: 1;
+    border-bottom: 1px solid #e0eaf5;
+  }
+  .separator:not(:empty)::before {
+    margin-right: .25em;
+  }
+  .separator:not(:empty)::after {
+    margin-left: .25em;
+  }
+  .content-box{
+    width: 40%;
+    padding: 20px;
+    margin: 5px 0px 5px 15px;
+    text-align: center;
+  }
+  .content-title{
+    margin-bottom: 15px;
+    font-weight: 900;
+    font-family: 'Archivo Black', sans-serif;
+  }
+  .content-parag{
+    font-size: 20px;
+    font-weight: 100;
+  }
+  .content-image{
+    margin-right: 30px;
+    padding-right: 20px;
+  }
 </style>
