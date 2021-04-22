7c7776075a4436fab926e4770918ef7c339c48ba
3 <77794481+smallpixel1105@users.noreply.github.com>
2021-04-22 21:05:31 +0300

    rearanjare card review

--- src/views/produs.vue
+++ src/views/produs.vue
@@ -1,191 +1,196 @@
 <template>
-    <v-main>
-      <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
-        <div style="height:350px;width:2000px;position: absolute;top: 0px;z-index: 1;" class="grey lighten-3"></div>
-        <navbar />
-        <v-progress-circular
-                :size="50"
-                color="primary"
-                indeterminate
-                v-if="loading"
-        ></v-progress-circular>
-        <br><br><br><br>
-        <div v-if="!loading" style="z-index:999;position: relative;margin:auto;width:70%">
-            <div>
-                <v-container>
-                    <v-row>
-                        <v-col>
-                            <div>
-                                <div class="stangaDivProd">
-                                    <img class="imagDesc" :src="theProd.img" alt="alt text" />
-                                    <div class="rat">
-                                        ({{theProd.rating}})</div>
-                                    <v-rating class="ste"
-                                              background-color="warning lighten-1"
-                                              color="warning"
-                                              half-increments
-                                              length="5"
-                                              readonly
-                                              size="25"
-                                              :value=theProd.rating
-                                    ></v-rating>
-                                    <v-card-title class="titlu">{{theProd.name}}</v-card-title>
-                                    <v-card-subtitle class="tex">Rewiews: {{theProd.reviews}}</v-card-subtitle>
-                                    <v-card-text class="tex">{{ theProd.descriere}}</v-card-text>
-                                    <div>
-                                        <v-btn :href="theProd.link" class="button text--black font-weight-bold" plain>
-                                            Link Produs
-                                        </v-btn>
-                                        <add-review-popup :Ids="Ids" v-if="userIsAuthenticated" />
-                                    </div>
-                                </div>
-                            </div>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </div>
-            <div style="margin:auto;width:60%">
-                <v-container>
-                    <v-row>
-                        <v-col>
-                            <v-card class="pa-7 mb-4 d-flex flex-no-wrap" v-for="review in reviews" :key="review.id">
-                                <div>
-                                    <div class="rat">
-                                        ({{review.rating}})</div>
-                                    <v-rating class="ste"
-                                              background-color="warning lighten-1"
-                                              color="warning"
-                                              half-increments
-                                              length="5"
-                                              readonly
-                                              size="25"
-                                              :value=review.rating
-                                    ></v-rating>
-                                    <v-avatar style="float: left">
-                                        <img :src="review.userImg" class="avatarr">
-                                    </v-avatar>
-                                    <v-card-title class="titlu">{{review.name}}</v-card-title>
-                                    <v-card-title class="texti">{{review.title}}</v-card-title>
-                                    <v-card-text class="tex">{{review.text | quotes}}</v-card-text>
-                                </div>
-                                <div><img class="imag" :src="review.img" /></div>
-                            </v-card>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </div>
-        </div>
-    </v-main>
+  <v-main>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+    <div style="height:350px;width:2000px;position: absolute;top: 0px;z-index: 1;" class="grey lighten-3"></div>
+    <navbar />
+    <v-progress-circular
+        :size="50"
+        color="primary"
+        indeterminate
+        v-if="loading"
+    ></v-progress-circular>
+    <br><br><br><br>
+    <div v-if="!loading" style="z-index:999;position: relative;margin:auto;width:70%">
+      <div>
+        <v-container>
+          <v-row>
+            <v-col>
+              <div>
+                <div class="stangaDivProd">
+                  <img class="imagDesc" :src="theProd.img" alt="alt text" />
+                  <div class="rat">
+                    ({{theProd.rating}})</div>
+                  <v-rating class="ste"
+                            background-color="warning lighten-1"
+                            color="warning"
+                            half-increments
+                            length="5"
+                            readonly
+                            size="25"
+                            :value=theProd.rating
+                  ></v-rating>
+                  <v-card-title class="titlu">{{theProd.name}}</v-card-title>
+                  <v-card-subtitle class="tex">Rewiews: {{theProd.reviews}}</v-card-subtitle>
+                  <v-card-text class="tex">{{ theProd.descriere}}</v-card-text>
+                  <div>
+                    <v-btn :href="theProd.link" class="button text--black font-weight-bold" plain>
+                      Link Produs
+                    </v-btn>
+                    <add-review-popup :Ids="Ids" v-if="userIsAuthenticated" />
+                  </div>
+                </div>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+      <br />
+      <div style="margin:auto;width:60%">
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-card class="pa-7 mb-4 d-flex flex-no-wrap" v-for="review in reviews" :key="review.id">
+                <div>
+                  <div style="float: right">
+                    <div class="rat">
+                      ({{review.rating}})</div>
+                      <v-rating class="ste"
+                                background-color="warning lighten-1"
+                                color="warning"
+                                half-increments
+                                length="5"
+                                readonly
+                                size="25"
+                                :value=review.rating
+                      ></v-rating>
+
+                  </div>
+
+                  <v-avatar style="float: left">
+                    <img :src="review.userImg" class="avatarr">
+                  </v-avatar>
+
+                  <v-card-title class="titlu">{{review.name}}</v-card-title>
+                  <div style="float: right;"><img class="imag" :src="review.img" /></div>
+                  <v-card-title class="texti">{{review.title}}</v-card-title>
+                  <v-card-text class="tex">{{review.text | quotes}}</v-card-text>
+
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </div>
+  </v-main>
 </template>
 
 <script>
-    import Navbar from "../components/Navbar";
-    import AddReviewPopup from "../components/AddReviewPopup";
-    export default {
-        name: "categorie",
-        props: ['catId','prodId'],
-        components: {
-            'navbar': Navbar,
-            'add-review-popup': AddReviewPopup
-        },
-        created() {
-            this.$store.dispatch('loadProd', {
-                catId: this.catId,
-                prodId: this.prodId
-            })
-            this.$store.dispatch('loadProductReviews', {
-                catId: this.catId,
-                prodId: this.prodId
-            })
-
-        },
-        data () {
-            return {
-                Ids: {
-                    IdCat: this.catId,
-                    IdProd: this.prodId
-                }
-            }
-        },
-        computed: {
-            reviews () {
-                return this.$store.getters.reviews
-            },
-            loading () {
-                return this.$store.getters.loading
-            },
-            theProd () {
-                return this.$store.getters.theProd
-            },
-            userIsAuthenticated () {
-                return this.$store.getters.user !== null && this.$store.getters.user !== undefined
-            }
-
-        }
+import Navbar from "../components/Navbar";
+import AddReviewPopup from "../components/AddReviewPopup";
+export default {
+  name: "categorie",
+  props: ['catId','prodId'],
+  components: {
+    'navbar': Navbar,
+    'add-review-popup': AddReviewPopup
+  },
+  created() {
+    this.$store.dispatch('loadProd', {
+      catId: this.catId,
+      prodId: this.prodId
+    })
+    this.$store.dispatch('loadProductReviews', {
+      catId: this.catId,
+      prodId: this.prodId
+    })
+  },
+  data () {
+    return {
+      Ids: {
+        IdCat: this.catId,
+        IdProd: this.prodId
+      }
     }
+  },
+  computed: {
+    reviews () {
+      return this.$store.getters.reviews
+    },
+    loading () {
+      return this.$store.getters.loading
+    },
+    theProd () {
+      return this.$store.getters.theProd
+    },
+    userIsAuthenticated () {
+      return this.$store.getters.user !== null && this.$store.getters.user !== undefined
+    }
+  }
+}
 </script>
 <style scoped>
-    .divProd{
-        z-index: 999;
-    }
-    .review{
-        margin-bottom:15px;
-    }
-    .imagDesc{
-        width:300px;
-        max-height: 220px;
-        max-width: 300px;
-        float:right;
-        margin: 20px;
-    }
-    .imag{
-        width:220px;
-        float: right;
-        margin-left: 130px
-    }
-    .texti{
-        padding:10px;
-        font-family: 'Lato', sans-serif;
-        font-weight: bold;
-    }
-    .tex{
-        font-size:15px;
-        padding:7px;
-        font-family: 'Lato', sans-serif;
-        font-weight: bold;
-
-    }
-
-    .titlu {
-      font-family: 'Archivo Black', sans-serif;
-      font-weight: bold;
-    }
-
-    .ste{
-        color: #ff9454;
-        float: right;
-        margin-top: 17px;
-    }
-    .rat{
-        float: right;
-        font-size: 20px;
-        padding: 3px;
-        margin-top: 13px;
-      font-family: 'Lato', sans-serif;
-      font-weight: bold;
-    }
-    .stangaDivProd{
-        margin-right:7%;
-    }
-    .button {
-        margin-bottom: 30px;
-        text-align: center;
-        background-color: hsl(47, 95%, 49%);
-    }
-    .avatarr{
-        float:left;
-        font-size:32px;
-        height: 42px;
-        width: 42px;
-    }
+.divProd{
+  z-index: 999;
+}
+.review{
+  margin-bottom:15px;
+}
+.imagDesc{
+  width:300px;
+  max-height: 220px;
+  max-width: 300px;
+  float:right;
+  margin: 20px;
+}
+.imag{
+  width:220px;
+  /*float: right;*/
+  /*margin-left: 130px;*/
+  justify-self: center;
+  display: flex;
+}
+.texti{
+  padding:10px;
+  font-family: 'lato', sans-serif;
+  font-weight: bolder;
+  font-size: x-large;
+}
+.tex{
+  font-size:15px;
+  padding:7px;
+  font-family: 'Lato', sans-serif;
+  font-weight: bolder;
+}
+.titlu {
+  font-family: 'Archivo Black', sans-serif;
+  font-weight: bolder;
+}
+.ste{
+  color: #ff9454;
+  float: right;
+  margin-top: 17px;
+}
+.rat{
+  float: right;
+  font-size: 20px;
+  padding: 3px;
+  margin-top: 13px;
+  font-family: 'Lato', sans-serif;
+  font-weight: bold;
+}
+.stangaDivProd{
+  margin-right:7%;
+}
+.button {
+  margin-bottom: 30px;
+  text-align: center;
+  background-color: hsl(47, 95%, 49%);
+}
+.avatarr{
+  float:left;
+  font-size:32px;
+  height: 42px;
+  width: 42px;
+}
 </style>
